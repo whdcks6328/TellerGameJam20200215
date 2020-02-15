c1cb18cf1fda2d517bbeb3d7a4d4d3443145b522
CHOCO\ChocoMing <kym6027@naver.com>
2020-02-15 19:42:47 +0900

    플레이어 작업중

--- TellerGameJam/Assets/Scripts/PlayerManager.cs
+++ TellerGameJam/Assets/Scripts/PlayerManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] float jump;
+    [SerializeField] float flingTime;
+    float skillTime;
+    float cooldown;
+    InputManager inputmanager;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(speed * Vector3.right);
+
+        if (inputmanager.CheckJumped() == true)
+        {
+            transform.Translate(jump * Vector3.up);
+        }
+
+        if (inputmanager.CheckColorChanged() == true)
+        {
+            transform.Translate(Vector3.up);
+            skillTime = 1f;
+            rb.useGravity = false;
+        }
+        if (skillTime > 0)
+        {
+            skillTime -= Time.deltaTime;
+        }
+        else rb.useGravity = true;
     }
 }
