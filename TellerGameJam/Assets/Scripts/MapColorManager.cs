42757d377928dc6aff635f0199302a05131713e9
Han Jongchan <whdcks6328@gmail.com>
2020-02-16 16:02:42 +0900

    혹시모를 백업

--- TellerGameJam/Assets/Scripts/MapColorManager.cs
+++ TellerGameJam/Assets/Scripts/MapColorManager.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MapColorManager : MonoBehaviour
 {
+    PlayerManager player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.isBGWhite)
+        {
+
+        }
+        else
+        {
+
+        }
     }
+    
 }
