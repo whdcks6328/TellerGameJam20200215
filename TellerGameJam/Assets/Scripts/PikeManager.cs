f14cbb8b6a3f421a85b126b11a98712a0aa44a29
CHOCO\ChocoMing <kym6027@naver.com>
2020-02-16 21:02:27 +0900

    가시/게임오버신

--- TellerGameJam/Assets/Scripts/PikeManager.cs
+++ TellerGameJam/Assets/Scripts/PikeManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class ScenesManager : MonoBehaviour
+
+public class PikeManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,9 +14,14 @@ public class ScenesManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene("HanTest");
+            //SceneManager.LoadScene("Gameover");
+            Debug.Log("Gameover");
         }
     }
 }
