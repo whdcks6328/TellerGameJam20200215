8620949db7b47ffd5d63b80f1d3828495a16beac
CHOCO\ChocoMing <kym6027@naver.com>
2020-02-16 15:08:01 +0900

    날아간 player 다시씀

--- TellerGameJam/Assets/Scripts/PlayerManager.cs
+++ TellerGameJam/Assets/Scripts/PlayerManager.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField] float speed;
-    [SerializeField] float jump;
-    [SerializeField] float flingTime;
-    float skillTime;
-    float cooldown;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float jump = 1f;
+    [SerializeField] float skillTime;
+    [SerializeField] float sCooldown = 0f;
+    [SerializeField] float jCooldowm = 0f;
     InputManager inputmanager;
     Rigidbody rb;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -21,23 +20,30 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed * Vector3.right);
+        sCooldown += Time.deltaTime;
+        jCooldowm += Time.deltaTime;
 
-        if (inputmanager.CheckJumped() == true)
-        {
-            transform.Translate(jump * Vector3.up);
+        transform.Translate(speed * Time.deltaTime * Vector3.right);
+
+        if(inputmanager.CheckJumped() == true && jCooldowm > 3f) 
+        { 
+            transform.Translate(jump * Time.deltaTime * Vector3.up);
+            jCooldowm = 0f;
         }
-
-        if (inputmanager.CheckColorChanged() == true)
+        if(inputmanager.CheckColorChanged() == true && sCooldown > 0.5f)
         {
-            transform.Translate(Vector3.up);
+            transform.Translate(Time.deltaTime * Vector3.up);
             skillTime = 1f;
             rb.useGravity = false;
         }
-        if (skillTime > 0)
+        if (skillTime > 0f)
         {
             skillTime -= Time.deltaTime;
         }
-        else rb.useGravity = true;
+        else
+        {
+            rb.useGravity = true;
+            sCooldown = 0f;
+        }
     }
 }
