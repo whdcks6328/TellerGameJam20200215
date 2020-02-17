2fee3a519a349ff252e695b8de28b063e9554ea9
Han Jongchan <whdcks6328@gmail.com>
2020-02-17 19:11:51 +0900

    음원 추가, 튜토리얼맵 완성, 별 앞에 Starzone 준비

---
+++ TellerGameJam/Assets/Scripts/SoundPlayer/SEZonePlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEZonePlayer : MonoBehaviour
+{
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        audio.Play();
+    }
+}
